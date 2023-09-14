e00b855d48ce451612c0c7514d4b930ade0946ea
baylorwroblewski <114018346+baylorwroblewski@users.noreply.github.com>
2023-09-13 21:37:55 -0400

    Week2Problem1

---
+++ Week2Problem1.java
@@ -0,0 +1,12 @@
+package cs176;
+
+public class Week2Problem1 {
+
+	public static void main(String[] args) {
+		System.out.println("\\(>_<)");
+		System.out.println("  ))z");
+		System.out.println("  / \\");
+
+	}
+
+}
